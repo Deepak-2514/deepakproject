feb7d41b583d038784b0ae6113f26ce2e1cf5a02
Deepak-2514 <kjdeepak2514@gmail.com>
2025-11-29 18:19:54 +0530

    .errorloading

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,7 +3,7 @@ import { ENV } from './lib/env.js'
 
 const app = express()
 
-app.get('/healh', (req, res) => {
+app.get('/health', (req, res) => {
   res.status(200).json({ msg: 'Api is running' })
 })
 
