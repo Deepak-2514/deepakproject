a9b9eb381830599b07f0b692aad036d18201cb40
Deepak-2514 <kjdeepak2514@gmail.com>
2025-11-29 17:52:17 +0530

    testing first api endpoint

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,10 +2,9 @@ import express from 'express'
 import { ENV } from './lib/env.js'
 
 const app = express()
-const Port = ENV.PORT
 
-app.get('/', (req, res) => {
-  res.status(200).json({ msg: 'success from backend' })
+app.get('/health', (req, res) => {
+  res.status(200).json({ msg: 'Api is running' })
 })
 
-app.listen(Port, console.log('server is running'))
+app.listen(ENV.PORT, console.log('server is running'))
