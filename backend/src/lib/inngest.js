006bdc874eb5a899ac9befae567a04f3b797f6a6
Deepak-2514 <kjdeepak2514@gmail.com>
2025-12-16 13:19:06 +0530

    Fix User model import path

--- backend/src/lib/inngest.js
+++ backend/src/lib/inngest.js
@@ -1,6 +1,6 @@
 import { Inngest } from "inngest";
 import { connectDB } from "./db.js";
-import User from "../modals/User";
+import User from "../modals/User.js";
 
 export const inngest = new Inngest({ id: "Interview" });
 
